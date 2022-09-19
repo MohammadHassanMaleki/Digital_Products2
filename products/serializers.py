fc27f5e3f4f198a37af03775a28a39e3b376016d
Hassan <m.maleki119@sadjad.ac.ir>
2022-09-19 16:02:15 +0430

    Products models added.

--- products/serializers.py
+++ products/serializers.py
@@ -5,17 +5,28 @@ from .models import Category, Product, File
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ('title', 'description', 'avatar')
+        fields = ('id', 'title', 'description', 'avatar')
 
 class FileSerializer(serializers.ModelSerializer):
+    file_type = serializers.SerializerMethodField()
     class Meta:
         model = File
-        fields = ('title', 'file')
+        fields = ('id','title', 'file', 'file_type')
+    def get_file_type(self, obj):
+        return obj.get_file_type_display()
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
     categories = CategorySerializer(many=True)
+    files = FileSerializer(many=True)
+    foo = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
-        fields = ('title','description', 'avatar', 'categories')
+        fields = ('id', 'title','description', 'avatar', 'categories', 'files', 'url', 'foo')
+
+    def get_foo(self, obj):
+        return obj.id
+
+
 
 
