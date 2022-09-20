c15412cb68e020c89c83cebb1f307ebe7feabe1a
Hassan <m.maleki119@sadjad.ac.ir>
2022-09-20 12:04:21 +0430

    costumized admin

--- Digital_Products2/settings.py
+++ Digital_Products2/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
 
     'products',
     'rest_framework',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authorization
+AUTH_USER_MODEL = 'users.User'
